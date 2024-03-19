24e5f6b07f10c4ed5aa3fd61a5e426b0a9162519
Swaraj Anand <swrjnnd@gmail.com>
2024-03-19 11:00:17 +0530

    Added the show more fns

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ SearchBtn.addEventListener("click", () => {
   console.log(input.value);
   let query = input.value;
   displayData(query);
+  mainContainer.innerHTML = "";
 });
 
 let page = 1;
 let clientId = "21mXJ2EbpBqB-XfQufUNEB1yvwqrVy84An5Iwcr8O4g";
-let query = "happy";
+let query = "eyes";
 
 const displayData = (query) => {
-  mainContainer.innerHTML = "";
+  // mainContainer.innerHTML = "";
+  if (query == "") {
+    query = "moon";
+  } else {
+    query = query;
+  }
   let api = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${clientId}`;
   let gridContainer = document.createElement("div");
   gridContainer.classList.add("gridContainer");
@@ -44,6 +50,11 @@ const displayData = (query) => {
 };
 
 displayData(query);
+
+showMoreBtn.addEventListener("click", () => {
+  page = page + 1;
+  displayData(query);
+});
 
 // console.log("Working");
 
