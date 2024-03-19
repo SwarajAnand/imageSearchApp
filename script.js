cd01588ef96126682507ad98374869822bc142bf
Swaraj Anand <swrjnnd@gmail.com>
2024-03-19 10:40:00 +0530

    added some files

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-// console.log("Working");
-// const input = document.getElementById("input");
-// const container = document.getElementById("container");
-// const showMoreBtn = document.getElementById("showMoreBtn");
-// const SearchBtn = document.getElementById("SearchBtn");
+console.log("Working");
+const input = document.getElementById("input");
+const mainContainer = document.getElementById("mainContainer");
+const showMoreBtn = document.getElementById("showMoreBtn");
+const SearchBtn = document.getElementById("SearchBtn");
 
-// SearchBtn.addEventListener("click", () => {
-//   console.log(input.value);
-//   let query = input.value;
-//   displayData(query);
-// });
+SearchBtn.addEventListener("click", () => {
+  console.log(input.value);
+  let query = input.value;
+  displayData(query);
+});
 
-// let query = input.value;
-let query = "Happy";
 let page = 1;
 let clientId = "21mXJ2EbpBqB-XfQufUNEB1yvwqrVy84An5Iwcr8O4g";
-
-// https://api.unsplash.com/search/photos?page=1&query=Happy&client_id=21mXJ2EbpBqB-XfQufUNEB1yvwqrVy84An5Iwcr8O4g
+let query = "happy";
 
 const displayData = (query) => {
+  mainContainer.innerHTML = "";
   let api = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${clientId}`;
+  let gridContainer = document.createElement("div");
+  gridContainer.classList.add("gridContainer");
   fetch(api)
     .then((res) => {
       //   console.log(res);
       return res.json();
     })
     .then((data) => {
-      //   console.log(data);
-      data.results.forEach((ele) => {
-        console.log(ele.id);
+      console.log(data);
+      data.results.forEach((ele, idx) => {
+        const demo = document.createElement("div");
+        demo.classList.add("container");
+        demo.innerHTML = `
+        <img src="${ele.urls.regular}" alt="${ele.alternative_slugs.en}"/>
+        `;
+
+        // gridContainer.appendChild(demo);
+        mainContainer.appendChild(demo);
       });
-      //     const demo = document.createElement("div");
-      //     demo.innerHTML = movie.id;
-      //     container.appendChild(demo);
-      //   });
+
+      // console.log(gridContainer);
+      // mainContainer.appendChild(gridContainer);
+      // console.log(mainContainer);
     });
 };
 
